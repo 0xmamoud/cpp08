17177e2d17a472b7ac619559236b53d1f530860d
mamoud <mamoud.antacid895@passmail.com>
2024-10-01 19:44:25 +0200

    ex02

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,26 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <stack>
+
+template <typename T, typename Container = std::deque<T>>
+class MutantStack : public std::stack<T, Container> {
+private:
+  /* data */
+
+public:
+  MutantStack() : std::stack<T, Container>() {}
+  MutantStack(const MutantStack &copy) : std::stack<T, Container>(copy) {}
+  MutantStack &operator=(const MutantStack &copy) {
+    std::stack<T, Container>::operator=(copy);
+    return *this;
+  }
+  virtual ~MutantStack() {}
+
+  typedef typename Container::iterator iterator;
+
+  iterator begin() { return this->c.begin(); }
+  iterator end() { return this->c.end(); }
+};
+
+#endif
